57df8d65b3b6858a71039dbfbccfccb44c7c71f2
Rifky Alamsyah <59393904+rifkyalamsyah@users.noreply.github.com>
2021-06-14 00:50:52 +0700

    Add files via upload
    C3

---
+++ C3/12.php
@@ -0,0 +1,16 @@
+<?php
+
+// Operator Logoka
+
+// And
+var_dump(true && true);
+var_dump(true && false);
+// Or
+var_dump(true || true);
+var_dump(true || false);
+// Not
+var_dump(!true);
+var_dump(!false);
+//Xor
+var_dump(true xor true);
+var_dump(true xor false);
